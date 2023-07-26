9a078bc08e4e04c655ad5b37cec14b8d664a85e4
Arterning <ning.huang.ridson@gmail.com>
2023-07-26 15:31:16 +0800

    add test

--- server/src/test/java/cn/ning/money/book/mapper/PermissionMapperTest.java
+++ server/src/test/java/cn/ning/money/book/mapper/PermissionMapperTest.java
@@ -1,24 +1,21 @@
-//package cn.ning.money.book.mapper;
-//
-//import cn.ning.money.book.entity.MenuDO;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.util.List;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class PermissionMapperTest {
-//    @Autowired
-//    private MenuMapper mapper;
-//
-//    @Test
-//    public void queryPermissionByUserId() {
-//        List<MenuDO> list = mapper.queryUserMenus(1L);
-//        Assert.assertEquals(3,list.size());
-//    }
-//}
+package cn.ning.money.book.mapper;
+
+import cn.ning.money.book.entity.MenuEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@SpringBootTest
+public class PermissionMapperTest {
+    @Autowired
+    private MenuMapper mapper;
+
+    @Test
+    public void queryPermissionByUserId() {
+        List<MenuEntity> menuEntities = mapper.queryUserMenus(1L);
+        System.out.println(menuEntities);
+    }
+
+}
